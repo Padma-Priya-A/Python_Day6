7fa9674cd2e3b6e2fc5fedb28ae06b8e968c9dac
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-09-30 22:38:32 +0530

    Add files via upload

---
+++ 10 nums from user find sum & avg.py
@@ -0,0 +1,13 @@
+a=[]
+for i in range(10):
+    num=int(input())
+    a.append(num)
+print(a)
+sum=0
+count=0
+for i in(a):
+    sum=sum+i
+    count=count+1
+    avg=sum/count
+print(sum)
+print(avg)
